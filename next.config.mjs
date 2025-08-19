22b52cc5581eccfcea0ab8d8bd7ab36c88419831
houssein-dev <houceinhmd@gmail.com>
2025-08-20 00:05:10 +0100

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export"
+    output:"export",
+    assetPrefix: '/my-app-train-deploy/',
 };
 
 export default nextConfig;
